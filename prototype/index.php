cd46658e928a72a3f2230135ecdaf39ddfe75135
Wlczak <adivlcak@seznam.cz>
2023-09-22 18:02:25 +0200

    Basic layout + img

---
+++ prototype/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <div class="grid">
+        <div class="grid-item1"></div>
+        <div class="grid-item2">
+            <!-- Logo-->
+            ><a class="logo">LOGO HERE</a>
+        </div>
+        <div class="grid-item3"></div>
+        <div class="grid-item4"></div>
+        <div class="content">
+            <div class="post">
+                <img src="" alt="user_logo">
+                <h3 class="username">username</h3>
+                <h4></h4>
+            </div>
+
+        </div>
+        <div class="grid-item6"></div>
+    </div>
+</body>
+
+</html>
