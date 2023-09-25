5171692304f9565c425b5be63345310c6fad9fe5
Wlczak <adivlcak@seznam.cz>
2023-09-25 22:31:10 +0200

    Register + styling

---
+++ register.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- import of set-up files -->
+    <?php
+    include("session/session_start.php");
+    setPage(1);
+    ?>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>>_Open\|/Book - Register</title>
+    <link rel="shortcut icon" href="logo/icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="style/style.css" />
+    <script src="js/script.js"></script>
+</head>
+
+<body>
+    <div class="grid">
+        <div class="grid-item1"></div>
+        <div class="grid-item2">
+            <!-- Logo-->
+            <div>
+                >_<a class="logo" href="/">Open\|/Book</a></div>
+            <a href="login.php" class="link">
+                <h2 id='login' onmouseenter="textAdd('login')" onmouseleave="stop_timeout()">
+                    <?php
+                    if ($_SESSION["logged_in"] == true) {
+                        echo "username_here";
+                    } else {
+                        echo "log in";
+                    }
+                    ?></h2>
+            </a>
+
+        </div>
+        <div class="grid-item3"></div>
+        <div class="grid-item4"></div>
+        <div class="grid-item5">
+            <div class="login">
+                <form method="get" class="login_form">
+                    <input type="text" name="username" class="input">
+                    <input type="password" name="password" class="input">
+                    <input type="submit" class="button" value=">_register">
+                </form>
+            </div>
+        </div>
+        <div class="grid-item6"></div>
+    </div>
+</body>
+
+</html>
