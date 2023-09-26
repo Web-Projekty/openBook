3e856450b87f2f8c0755d22c44c44e606868c614
Wlczak <adivlcak@seznam.cz>
2023-09-26 19:41:22 +0200

    Registration complete

--- register.php
+++ register.php
@@ -42,7 +42,13 @@
                     <input type="text" name="username" class="input" value="">
                     <input type="password" name="password" class="input">
                     <input type="submit" class="button" value=">_register">
-                    <p style="display: none;" id="warning" class="center"></p>
+                    <p style="display: none;" id="warning" class="center">
+                        <?php
+                        #hidden if js doesn't change dislay property 
+                        if ($_GET["username"] != null) echo "this user stole your username: <a id='user_link' href= '' >" . $_GET["username"] . "</a>";
+
+                        ?>
+                    </p>
                 </form>
             </div>
         </div>
@@ -54,43 +60,65 @@
 #msg declare
 $_SESSION["msg"] = "";
 #form data check
-if ($_GET["username"] == null || $_GET["password"] == null) {
-    if ($_GET["username"] == null) {
-        $_SESSION["msg"] .= "username, ";
-    }
-    if ($_GET["password"] == null) {
-        $_SESSION["msg"] .= "password";
-    }
-}
-if ($_SESSION["msg"] != null) {
-    echo "<script>
+if (!empty($_GET["username"]) || !empty($_GET["password"])) {
+
+
+    if ($_GET["username"] == null || $_GET["password"] == null) {
+        if ($_GET["username"] == null) {
+            $_SESSION["msg"] .= "username, ";
+        }
+        if ($_GET["password"] == null) {
+            $_SESSION["msg"] .= "password";
+        }
+    } else {
+        #import of mySQL config file
+        include("db_config/mySQL_config.php");
+
+        #mySQL connection
+        $mySQL = mysqli_connect($host, $user, $password, $database);
+
+
+        #form data handling
+        $username = $_GET["username"];
+        $password = hash("snefru", $_GET["password"]);
+        //var_dump($password);
+        //echo $password;
+
+        #username check
+        $query = "SELECT userID FROM user WHERE userName = '$username'";
+        #runs SQL query
+        $query_return = mysqli_query($mySQL, $query);
+        #makes associative array
+        $query_result = mysqli_fetch_assoc($query_return);
+        #user exists check
+        if ($query_result != NULL) {
+            #script for "this user stole your username" message
+            $msg = "this user stole your username: ";
+            echo "<script>
     var element1 = document.getElementById('form_border')
     element1.style.border = '2px red dashed'
     var element2 = document.getElementById('warning')
-    element2.innerHTML = '>_missingElements[".$_SESSION["msg"]."]'
+    element2.style.display = ''
+    var element3 = document.getElementById('user_link')
+    element3.href = '" ./*insert user page address -> */ "here/" . $query_result["userID"] . "'
+</script>";
+        } else {
+            $query = "INSERT INTO `user` (`userID`, `userName`, `userImgID`, `password`) VALUES (NULL, '" . $username . "', NULL, '" . $password . "')";
+            $query_return = mysqli_query($mySQL, $query);
+        }
+    }
+    if ($_SESSION["msg"] != null) {
+        #script for fucked up form
+        echo "<script>
+    var element1 = document.getElementById('form_border')
+    element1.style.border = '2px red dashed'
+    var element2 = document.getElementById('warning')
+    element2.innerHTML = '>_missingElements[" . $_SESSION["msg"] . "]'
     element2.style.display = ''
 </script>";
+    }
 }
 
-#import of mySQL config file
-include("db_config/mySQL_config.php");
-
-#mySQL connection
-$mySQL = mysqli_connect($host, $user, $password, $database);
-
-
-#form data handling
-$username = $_GET["username"];
-$password = hash("snefru", $_GET["password"]);
-//echo $password;
-
-#username check
-$query = "SELECT userName FROM user WHERE userName = '$username'";
-#runs SQL query
-$query_return = mysqli_query($mySQL, $query);
-#makes associative array
-$query_result = mysqli_fetch_assoc($query_return);
-#
 
 ?>
 
