078c01523889410005de4180cb586e299a5fc3f9
Wlczak <adivlcak@seznam.cz>
2023-10-12 21:54:39 +0200

    Paging system completed 👏

--- index.php
+++ index.php
@@ -49,54 +49,117 @@
         <div class="content">
             <?php
 
-            #import of mySQL config file
-            include("db_config/mySQL_config.php");
-
-            #mySQL connection and data SELECT
-            $mySQL = mysqli_connect($host, $user, $password, $database);
-            $query = "SELECT * FROM post ORDER BY date DESC";
-            $query_return = mysqli_query($mySQL, $query);
-
-            #assign number of posts before data processsing
-            $postNumber = mysqli_num_rows($query_return);
-
-            #mySQL data output
-            while ($query_result = mysqli_fetch_assoc($query_return)) {
-
-                #get username
-                $query_user = "SELECT * FROM user WHERE userID = " . $query_result["userID"];
-                $query_result_user = mysqli_fetch_assoc(mysqli_query($mySQL, $query_user));
-
-                #data output
-                echo "
-            
-                <div class='post_container'>
-                    <img src='img/profile/profile_" . $query_result_user["userImgID"] . ".png' alt='user_logo' class='profile_logo'>
-                    <h3 class='username'>" . $query_result_user["userName"] . "</h3>
-                    <h4>" . $query_result["postName"] . "</h4>
-                    <img class='post' src='img/post/post_" . $query_result["postID"] . ".png' alt='post_img'>
-                </div>
-            ";
-            };
-
-            mysqli_close($mySQL);
-            echo ceil(($postNumber / 5));
-
-            #page echo
-            $pageCount = ceil($postNumber / 5);
-
-            if ($pageCount > 1) {
-                echo "<footer>
-                    <div class='pages'>";
-                for ($i = 1; $i <= $pageCount; $i++) {
-                    echo "<a class='pages-text' href='/index.php?page=".$i."'>".$i."</a>";
-                }
+            if (isset($_GET["page"])) {
+                $page = $_GET["page"];
+            } else {
+                $page = 1;
             }
-            echo "</div>
+            if (isset($page)) {
+                #import of mySQL config file
+                include("db_config/mySQL_config.php");
+
+                #mySQL connection and data SELECT
+                $mySQL = mysqli_connect($host, $user, $password, $database);
+                $query = "SELECT * FROM post ORDER BY date DESC";
+                $query_return = mysqli_query($mySQL, $query);
+
+                #assign number of posts before data processsing
+                $postNumber = mysqli_num_rows($query_return);
+
+                #setting post limit & counter
+                $maxPost = $page * 5;
+                $minPost = $maxPost - 4;
+                $postId = 0;
+
+                #mySQL data output
+                while ($query_result = mysqli_fetch_assoc($query_return)) {
+                    $postId++;
+                    /* debug
+
+                    echo "<br>post id: ";echo $postId;
+                    echo "<br> min post:";
+                    echo $minPost;
+                    echo "<br>maxpost:";
+                    echo $maxPost; */
+
+                    if ($postId >= $minPost && $postId <= $maxPost) {
+
+                        #get username
+                        $query_user = "SELECT * FROM user WHERE userID = " . $query_result["userID"];
+                        $query_result_user = mysqli_fetch_assoc(mysqli_query($mySQL, $query_user));
+
+                        #data output
+                        echo "
+
+                            <div class='post_container'>
+                                <img src='img/profile/profile_" . $query_result_user["userImgID"] . ".png' alt='user_logo' class='profile_logo'>
+                                <h3 class='username'>" . $query_result_user["userName"] . "</h3>
+                                <h4>" . $query_result["postName"] . "</h4>
+                                <img class='post' src='img/post/post_" . $query_result["postID"] . ".png' alt='post_img'>
+                            </div>
+                        ";
+                    }
+                };
+
+                mysqli_close($mySQL);
+
+                #pageCount declaration
+                $pageCount = ceil($postNumber / 5);
+
+                if ($pageCount > 1) {
+                    echo "<footer>
+                    <div class='pages'>";
+
+                    #selective declaration of $i
+
+                    $minPage = $page - 2;
+                    $maxPage = $page + 2;
+                    while ($minPage < 1 || $maxPage > $pageCount) {
+                        #debug
+                        echo "<br>max:" . $maxPage . "<br>min:" . $minPage;
+                        if ($minPage < 1) {
+                            $minPage++;
+                        }
+                        if ($maxPage > $pageCount) {
+                            $maxPage--;
+                        }
+                    }
+                    if ($page < 3) {
+                        echo "loloec";
+                        while ($maxPage != $pageCount) {
+                            if ($maxPage - $minPage == 4) {
+                                break;
+                            }
+                            $maxPage++;
+                        }
+                    }
+                    if ($pageCount - $page < 3) {
+                        echo "trolec";
+                        while ($maxPage - $minPage != 4) {
+                            $minPage--;
+                            if ($minPage == 0) {
+                                $minPage++;
+                                break;
+                            }
+                        }
+                    }
+
+                    for ($i = $minPage; $i <= $maxPage; $i++) {
+                        #show selected page
+                        if ($i == $page) {
+                            echo "<a class='pages-text' href='/index.php?page=" . $i . "' style=' font-weight: bold'>" . $i . "</a>";
+                        } else {
+                            echo "<a class='pages-text' href='/index.php?page=" . $i . "'>" . $i . "</a>";
+                        }
+                    }
+                }
+                echo "</div>
                 </footer>";
+            }
+
 
             ?>
-           <!-- footer template
+            <!-- footer template
              <footer>
                 <div class="pages">
                     <p class="pages-text">lol</p>
